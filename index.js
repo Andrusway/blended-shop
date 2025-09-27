7dc53c75b53502634f309f5e5f194f918533ef73
IrynaYermak <1991irenka@gmail.com>
2025-09-27 13:09:40 +0000

    Deploying to gh-pages from @ Andrusway/blended-shop@6308ef04ad42ae4fd926794b0e8f57013285ca66 🚀

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import"./assets/styles-JE8YjOlG.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";const c="https://dummyjson.com",a={CATEGORIES:"/products/category-list"};o.defaults.baseURL=c;async function r(){const{data:t}=await o(`${a.CATEGORIES}`);return t}const n={categoryList:document.querySelector(".categories")};function i(t){const e=t.map(s=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${s}</button>
- </li>`).join("");n.categoryList.innerHTML=e}async function u(){try{const t=await r();i(["All",...t]);const e=document.querySelector(".categories__btn");e&&e.classList.add("categories__btn--active")}catch(t){console.log(t)}}u();
+import"./assets/styles-JE8YjOlG.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";const n="https://dummyjson.com",e=12,a={CATEGORIES:"/products/category-list",PRODUCTS:"/products"};o.defaults.baseURL=n;async function i(){const{data:t}=await o(`${a.CATEGORIES}`);return t}async function u(t){const s={params:{skip:(t-1)*e,limit:e}},{data:c}=await o(a.PRODUCTS,s);return c}const r={categoryList:document.querySelector(".categories"),productList:document.querySelector("ul.products")};function d(t){const s=t.map(c=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${c}</button>
+ </li>`).join("");r.categoryList.innerHTML=s}function p(t){const s=t.products.map(c=>`<li class="products__item" data-id="${c.id}">
+    <img class="products__image" src="${c.thumbnail}" alt="product image"/>
+    <p class="products__title">${c.title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: </span>${c.brand}</p>
+    <p class="products__category">Category: ${c.category}</p>
+    <p class="products__price">Price: $${c.price}</p>
+ </li>`).join("");r.productList.insertAdjacentHTML("beforeend",s)}async function l(){try{const t=await i();d(["All",...t]);const s=document.querySelector(".categories__btn");s&&s.classList.add("categories__btn--active")}catch(t){console.log(t)}}async function _(t){try{const s=await u(t);p(s)}catch(s){console.log(s)}}l();_(1);
 //# sourceMappingURL=index.js.map
